dff6c2afc3afdb16bf8e3cc75b02eb9bd4fc0c0b
Jacqueline Sanchez <Jacquelinesanchez4712@gmail.com>
2026-01-27 02:22:01 -0600

    updating links

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { renderHero } from "./components/hero/hero-threads.js";
-import { initScrollSpy } from "./scrollspy.js";
+import { initScrollSpy } from "./components/scrollspy.js";
 import { initMenu } from "./menu.js";
 
 const hero = document.getElementById("hero");
