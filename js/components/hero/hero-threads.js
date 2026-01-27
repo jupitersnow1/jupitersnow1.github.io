dd269d47a0ee71f13c9f758d08b8e85dd1cb4e81
Jacqueline Sanchez <Jacquelinesanchez4712@gmail.com>
2026-01-27 01:29:01 -0600

    refractoring hero section

---
+++ js/components/hero/hero-threads.js
@@ -0,0 +1,6 @@
+export function renderHero(container) {
+  container.innerHTML = `
+    <h1>Howdy!</h1>
+    <p>hero coming soon!</p>
+  `;
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { renderHero } from "./components/hero/hero-threads.js";
+
+const hero = document.getElementById("hero");
+renderHero(hero);
